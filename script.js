8a965e591707e1eecfd8676f64ca44ff6709e4aa
Naikin123 <an3038489@gmail.com>
2026-02-01 00:50:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+const btn = document.getElementById("nextBtn");
+
+btn.addEventListener("click", () => {
+  document.body.style.background = "#000";
+  btn.textContent = "NO HAY REGRESO";
+
+  setTimeout(() => {
+    alert("Registro incompleto.\nLa habitación no estaba vacía.");
+  }, 1000);
+});
